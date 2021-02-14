c43b0ee45b1173d6754e9001eb890dec7ba23300
Syukri Ghazali <syukrideveloper@gmail.com>
2021-02-14 13:18:17 +0800

    Adding email composer

--- ionicStarWarsApp/src/app/app.module.ts
+++ ionicStarWarsApp/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http'
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  EmailComposer],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- ionicStarWarsApp/src/app/pages/film-details/film-details.page.ts
+++ ionicStarWarsApp/src/app/pages/film-details/film-details.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 import { ApiService } from 'src/app/services/api.service';
 
 @Component({
@@ -11,13 +12,23 @@ export class FilmDetailsPage implements OnInit {
   // filmId = null;
   film: any;
   
-  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService, private emailComposer: EmailComposer) { }
 
   ngOnInit() {
     let id = this.activatedRoute.snapshot.paramMap.get('id');
     this.api.getFilm(id).subscribe(res => {
       this.film = res;
     })
+  }
+
+  shareFilm() {
+    let email = {
+      to: 'syukrighazali001@gmail.com',
+      subject: "I love this one: " + this.film.title,
+      body: 'Can you remember the opening? <br><br>\"' + this.film.opening_crawl + '\"',
+      isHtml: true
+    };
+    this.emailComposer.open(email);
   }
 
 }
