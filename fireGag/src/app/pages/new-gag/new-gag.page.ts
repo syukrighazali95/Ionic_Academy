19c2f5fc23e3a6a0fbf0438cb5a050bc3cd029fb
Syukri Ghazali <syukrideveloper@gmail.com>
2021-02-20 17:17:02 +0800

    adding image box for image upload

--- fireGag/src/app/pages/new-gag/new-gag.page.ts
+++ fireGag/src/app/pages/new-gag/new-gag.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { DataService, Gag } from 'src/app/services/data.service';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+const { Camera } = Plugins
+
 
 @Component({
   selector: 'app-new-gag',
@@ -16,9 +19,23 @@ export class NewGagPage implements OnInit {
     creator: null
   }
 
+  capturedImage = null;
+
   constructor(private router: Router, private dataService: DataService, private loadingController: LoadingController) { }
 
   ngOnInit() {
+  }
+
+  async addImage() {
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: true,
+      source: CameraSource.Photos,
+      resultType: CameraResultType.Base64
+    });
+    console.log('result: ', image)
+    this.capturedImage = `data:image/jpeg:base64, ${image.base64String}`;
+    this.gag.image = image.base64String;
   }
 
   async save() {
