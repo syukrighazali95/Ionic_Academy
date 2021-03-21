81ed7ae800c60443bba8af6bacf95fe2eb8fd3f0
Syukri Ghazali <syukrideveloper@gmail.com>
2021-03-21 20:41:27 +0800

    Add complete paramExample

--- paramExample/src/app/pages/popover/popover.page.ts
+++ paramExample/src/app/pages/popover/popover.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavParams, PopoverController } from '@ionic/angular';
 
 @Component({
   selector: 'app-popover',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popover.page.scss'],
 })
 export class PopoverPage implements OnInit {
-
-  constructor() { }
+  passedId = null;
+  constructor(private navParam: NavParams, private popoverController: PopoverController) { }
 
   ngOnInit() {
+    this.passedId = this.navParam.get("custom_id");
+  }
+
+  closePopover() {
+    this.popoverController.dismiss();
   }
 
 }
