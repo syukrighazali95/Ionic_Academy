628b8a8e35e7015b6cc20f253d77ac2cd6dc4500
Syukri Ghazali <syukrideveloper@gmail.com>
2021-03-21 19:52:45 +0800

    Adding complete home page in paramExample

--- paramExample/src/app/app-routing.module.ts
+++ paramExample/src/app/app-routing.module.ts
@@ -12,17 +12,10 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'second',
+    path: 'second/:myid',
     loadChildren: () => import('./pages/second/second.module').then( m => m.SecondPageModule)
-  },
-  {
-    path: 'modal',
-    loadChildren: () => import('./pages/modal/modal.module').then( m => m.ModalPageModule)
-  },
-  {
-    path: 'popover',
-    loadChildren: () => import('./pages/popover/popover.module').then( m => m.PopoverPageModule)
-  },
+  }
+  
 ];
 
 @NgModule({
--- paramExample/src/app/app.module.ts
+++ paramExample/src/app/app.module.ts
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ModalPageModule } from './pages/modal/modal.module';
+import { PopoverPageModule } from './pages/popover/popover.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  ModalPageModule, PopoverPageModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
