4ace030e9f065f99492d0ed856b05c187af6cbaa
Syukri Ghazali <syukrideveloper@gmail.com>
2021-03-21 20:19:09 +0800

    Adding home page typescript for paramExample

--- paramExample/src/app/home/home.page.ts
+++ paramExample/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ModalController, NavController, PopoverController } from '@ionic/angular';
+import { ModalPage } from '../pages/modal/modal.page';
+import { PopoverPage } from '../pages/popover/popover.page';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  value = 0;
 
-  constructor() {}
+  constructor(private nav: NavController, private modalController: ModalController, private poppverController: PopoverController) {}
 
+  pushPage() {
+    this.nav.navigateForward(`/second/${this.value}`);
+  }
+
+  async openModal() {
+    const modal = await this.modalController.create({
+      component: ModalPage,
+      componentProps: {
+        custom_id: this.value
+      }
+    });
+    await modal.present();
+  }
+
+  async openPopover(ev: Event) {
+    const popover = await this.poppverController.create({
+      component: PopoverPage,
+      event: ev,
+      componentProps: {
+        custom_id: this.value
+      }
+    });
+    await popover.present();
+  }
 }
