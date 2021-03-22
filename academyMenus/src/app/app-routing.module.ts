1a70a913f5a1da93a934bbcc0e4ea6957d7815a9
Syukri Ghazali <syukrideveloper@gmail.com>
2021-03-22 20:32:34 +0800

    Adding pages for academyMenus

--- academyMenus/src/app/app-routing.module.ts
+++ academyMenus/src/app/app-routing.module.ts
@@ -5,6 +5,22 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'news',
+    loadChildren: () => import('./news/news.module').then( m => m.NewsPageModule)
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then( m => m.UserPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
   }
 ];
 @NgModule({
