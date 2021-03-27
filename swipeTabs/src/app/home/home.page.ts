8ab1728f7d1c76aa63d9d218ee23925a2978f68b
Syukri Ghazali <syukrideveloper@gmail.com>
2021-03-27 08:29:05 +0800

    Update home page for swipeTabs

--- swipeTabs/src/app/home/home.page.ts
+++ swipeTabs/src/app/home/home.page.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonNav } from '@ionic/angular';
 import { AboutPage } from '../about/about.page';
 import { AccountPage } from '../account/account.page';
 import { NewsPage } from '../news/news.page';
+import { SuperTabChangeEventDetail } from '@ionic-super-tabs/core';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +16,35 @@ export class HomePage {
   aboutPage = AboutPage;
 
   title = "News";
+  canGoBack = false;
+
+  @ViewChild('newsNav', {static: false}) newsNav: IonNav;
 
   constructor() {}
+
+  ngAfterViewInit() {
+    this.newsNav.ionNavDidChange.subscribe(async ev => {
+      this.canGoBack = await this.newsNav.canGoBack();
+      console.log(this.canGoBack);
+    })
+  }
+
+  onTabChange(ev: CustomEvent<SuperTabChangeEventDetail>){
+    switch (ev.detail.index) {
+      case 0:
+        this.title = "News";
+        break;
+      case 1:
+        this.title = "Account";
+        break;
+      case 2:
+        this.title = "About";
+        break;
+    }
+  }
+
+  goBack() {
+    this.newsNav.pop();
+  }
 
 }
