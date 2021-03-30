cd29131b7badd7cb8041bf0d2d864a3bd827c122
Syukri Ghazali <syukrideveloper@gmail.com>
2021-03-30 21:59:01 +0800

    Update loginpage in academyDesign

--- academyDesign/src/app/app.module.ts
+++ academyDesign/src/app/app.module.ts
@@ -9,11 +9,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { IonicStorageModule } from '@ionic/storage';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fab);
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
-  HttpClientModule, IonicStorageModule.forRoot()],
+  HttpClientModule, IonicStorageModule.forRoot(), FontAwesomeModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- academyDesign/src/app/pages/login/login.module.ts
+++ academyDesign/src/app/pages/login/login.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    FontAwesomeModule
   ],
   declarations: [LoginPage]
 })
