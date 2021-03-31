796d47f6b40fc94fbe311f0c0213817ce9b41ca3
Syukri Ghazali <syukrideveloper@gmail.com>
2021-03-31 19:02:46 +0800

    Update pages in academyDesign

--- academyDesign/src/app/app-routing.module.ts
+++ academyDesign/src/app/app-routing.module.ts
@@ -17,6 +17,22 @@ const routes: Routes = [
     path: 'tutorial',
     loadChildren: () => import('./pages/tutorial/tutorial.module').then( m => m.TutorialPageModule)
   },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'news',
+    loadChildren: () => import('./pages/news/news.module').then( m => m.NewsPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
 ];
 
 @NgModule({
