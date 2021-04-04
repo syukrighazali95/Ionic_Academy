a1ca1433eb16b42c9125bb691fb7319f79cdc200
Syukri Ghazali <syukrideveloper@gmail.com>
2021-04-04 17:43:42 +0800

    Adding complete academyNotifications

--- academyNotifications/src/app/app.module.ts
+++ academyNotifications/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  LocalNotifications],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- academyNotifications/src/app/home/home.page.ts
+++ academyNotifications/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ELocalNotificationTriggerUnit, ILocalNotification, LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { AlertController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,64 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  scheduled = [];
 
-  constructor() {}
+  constructor(private plt: Platform, private localNotification: LocalNotifications, private alertCtrl: AlertController) {
+    this.plt.ready().then(() => {
+      this.localNotification.on('click').subscribe(res => {
+        let msg = res.data ? res.data.mydata : '';
+        this.showAlert(res.title, res.text, msg);
+      });
+
+      this.localNotification.on('trigger').subscribe(res => {
+        let msg = res.data ? res.data.mydata : '';
+        this.showAlert(res.title, res.text, msg);
+      })
+    })
+  }
+
+  showAlert(header, sub, msg) {
+    this.alertCtrl.create({
+      header: header,
+      subHeader: sub,
+      message: msg,
+      buttons: ["Ok"]
+    }).then(alert => alert.present());
+  }
+
+  scheduleNotification() {
+    this.localNotification.schedule({
+      id: 1,
+      title: 'Attention',
+      text: 'Simon Notification',
+      data: { mydata: 'My hidden message this is'},
+      trigger: { in: 5, unit: ELocalNotificationTriggerUnit.SECOND},
+      foreground: true
+    })
+  }
+
+  recurringNotification() {
+    this.localNotification.schedule({
+      id: 22,
+      title: 'Recurring',
+      text: 'Simons Recurring Notification',
+      trigger: { every: ELocalNotificationTriggerUnit.MINUTE}
+    });
+  }
+
+  repeatingDaily() {
+    this.localNotification.schedule({
+      id: 42,
+      title: 'Good Morning',
+      text: 'Code something epic today!',
+      trigger: { every: {hour: 9, minute: 30}}
+    })
+  }
+
+  getAll() {
+    this.localNotification.getAll().then((res: ILocalNotification[]) => {
+      this.scheduled = res;
+    })
+  }
 
 }
