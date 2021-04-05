8c09354e81842828a7167de272df26529063ac63
Syukri Ghazali <syukrideveloper@gmail.com>
2021-04-05 21:37:09 +0800

    Update the socialShare project

--- socialShare/src/app/app.module.ts
+++ socialShare/src/app/app.module.ts
@@ -6,12 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { File } from '@ionic-native/file/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  SocialSharing, File],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- socialShare/src/app/home/home.page.ts
+++ socialShare/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { File } from '@ionic-native/file/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  text = "Check out the Ionic Academy";
+  url = "https://ionicacademy.com";
 
-  constructor() {}
+  constructor(private socialSharing: SocialSharing, private file: File) {}
+
+  async shareTwitter(){
+    this.socialSharing.shareViaTwitter(null, null, this.url).then(() => {
+
+    }).catch((e) => {
+
+    });
+  }
+
+  async shareWhatsapp() {
+    this.socialSharing.shareViaWhatsApp(this.text,null,this.url).then(() => {
+
+    }).catch((e) => {
+
+    });
+  }
+
+  async resolveLocalFile(){
+    return this.file.copyFile(`${this.file.applicationDirectory}www/assets/imgs`, 'person.jpg', this.file.cacheDirectory, `${new Date().getTime()}.jpg`)
+  }
+
+  removeTempFile(name) {
+    this.file.removeFile(this.file.cacheDirectory, name)
+  }
+
+  async shareEmail() {
+    let file = await this.resolveLocalFile();
+    this.socialSharing.shareViaEmail(this.text, 'My custom subject', ['syukrighazali95@gmail.com'], null, null, file.nativeURL).then(() => {
+      this.removeTempFile(file.name);
+    }).catch((e) => {
+
+    });
+  }
+
+  async shareFacebook(){
+    let file = await this.resolveLocalFile();
+    this.socialSharing.shareViaFacebook(null, file.nativeURL, null).then(() => {
+      this.removeTempFile(file.name);
+    }).catch((e) => {
+
+    });
+  }
 
 }
