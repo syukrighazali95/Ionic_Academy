e4fde43b538a59bcaf9e5d73ab1de136f44e5edb
Syukri Ghazali <syukrideveloper@gmail.com>
2021-04-06 22:41:38 +0800

    Update acedemyYelp project

---
+++ academyYelp/src/environments/environment.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  yelpKey: 'Ihl9NQCi1T74WtiiGYSViBgqqIz7qdajdXNCQ4ddaYXqE1xz90qeBYHOZTi8fOu8oBSWsab9GUonxmRmz6TocNKn__rli9JtAJCfOJbdVJkleki8H6LYOPk_JVJsYHYx',
+  mapKey: 'AIzaSyAXeftu71Ca88UfVKlA4WIVA3frm204DRo'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
